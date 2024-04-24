3a3d0bf5b1837538666f0294606aa0f83d0c32c4
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-24 16:30:35 +0530

    data has been updated for schedule round1

--- js/scheduleSheet.js
+++ js/scheduleSheet.js
@@ -65,8 +65,8 @@ const getDataFromGoogleSheets2 = (SHEET_ID,RANGE,SHEET_TITLE,tableNo) => {
       });
   }
   
-  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B242:E244","Dec 23 Agency PK Battle","1");
-  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B246:E248","Dec 23 Agency PK Battle","2");
-  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B250:E252","Dec 23 Agency PK Battle","3");
-  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B268:E270","Dec 23 Agency PK Battle","4");
-  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B272:E274","Dec 23 Agency PK Battle","5");
+  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B325:E327","Dec 23 Agency PK Battle","1");
+  getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B329:E331","Dec 23 Agency PK Battle","2");
+  // getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B250:E252","Dec 23 Agency PK Battle","3");
+  // getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B268:E270","Dec 23 Agency PK Battle","4");
+  // getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B272:E274","Dec 23 Agency PK Battle","5");
