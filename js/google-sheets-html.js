1f2c652da25318527054af9cbc0d8035cfc87435
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-27 11:43:16 +0530

    leaderboard round1

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -111,11 +111,11 @@ function rowsCount(item){
  
   dataList.forEach(dataCount);
   console.log("counter is"+counter);
-  if(counter>=0   &&  counter<0){
+  if(counter>=0   &&  counter<10){
      $('#conDiv').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
 
   }
-  else  if(counter>=0  &&  counter<12){
+  else  if(counter>=10  &&  counter<22){
 
 
   
@@ -125,7 +125,7 @@ function rowsCount(item){
 
   }
 
-  else   if(counter>=12  &&  counter<=15){
+  else   if(counter>=22  &&  counter<=25){
 $('#agencyrec').append('<div class="colThreelast"><div>Id: '+id+'</div><div class="centerDivlast"><img src="img/coin.png" class="imgsmal">'+name+'<br></div><div class="lastDiv"><span class="dancing">'+times+'</span><p></p></div></div>');
 
  
